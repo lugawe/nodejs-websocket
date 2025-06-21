7012a7a44d82d3b228a538aafc396f4e17ba25db
Luis Weich <luis.weich.de@gmail.com>
2025-06-21 14:42:56 +0200

    format socket-server.js

--- ws/socket-server.js
+++ ws/socket-server.js
@@ -1,30 +1,25 @@
-import { handleClientScriptLoad } from "next/script";
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from "ws"
 
-// 서버인스턴스 만들고, 3100포트에서 클라이언트 연결 기다림
-const wss = new WebSocketServer({ port: 3100 });
-console.log("Server is running. port: 3100");
+const port = 3100
 
-// 클라이언트가 서버에 연결할때 발생
+const wss = new WebSocketServer({ port: port })
+console.log("WebSocketServer is running on port " + port)
+
 wss.on("connection", (socket) => {
-  console.log("Client connected");
+  console.log("new client connected")
 
-  // 클라이언트가 서버에 메세지 보낼때 발생
   socket.on("message", (message) => {
-    console.log("Received message:", message.toString());
-    sendToAll(message);
-});
-});
+    console.log("received message: ", message.toString())
+    sendToAll(message)
+  })
+})
 
 function sendToAll(message) {
-
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(message.toString());
+      client.send(message.toString())
     }
-  });
+  })
 }
 
-
-
-export default wss;
+export default wss
